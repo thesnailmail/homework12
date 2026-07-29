2803e58de6f956b566781f124d010a739bf8b296
Мария <thesnailmail@icloud.com>
2026-07-30 02:59:21 +0300

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        //«Преступление и наказание», Фёдор Михайлович Достоевский, 1866 год.
+        //«Великий Гэтсби», Фрэнсис Скотт Фицджеральд, 1925 год.
+        //«451° по Фаренгейту», Рэй Брэдбери, 1953 год.
+        Authors firstAuthor = new Authors("Фёдор", "Достоевский");
+        Authors secondAuthor = new Authors("Фрэнсис", "Фицджеральд");
+        Authors thirdAuthor = new Authors("Рэй", "Брэдбери");
+        Book firstBook = new Book("Преступление и наказание", firstAuthor, 1866);
+        Book secondBook = new Book("Великий Гэтсби", secondAuthor, 1925);
+        Book thirdBook = new Book("451° по Фаренгейту", thirdAuthor, 0);
+
+        System.out.println(firstBook.getNameOfTheBook());
+        System.out.println(firstBook.getAuthor());
+        System.out.println(firstBook.getYearOfPublish());
+
+        System.out.println(secondBook.getNameOfTheBook());
+        System.out.println(secondBook.getAuthor());
+        System.out.println(secondBook.getYearOfPublish());
+
+        thirdBook.setYearOfPublish(1953);
+
+        System.out.println(thirdBook.getNameOfTheBook());
+        System.out.println(thirdBook.getAuthor());
+        System.out.println(thirdBook.getYearOfPublish());
     }
 }
