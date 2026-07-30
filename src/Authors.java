e84fbe4058bd0d09bbad8d7c4debd2a65fb92258
Мария <thesnailmail@icloud.com>
2026-07-30 03:01:35 +0300

    Initial commit

--- src/Authors.java
+++ src/Authors.java
@@ -1,17 +1,3 @@
 public class Authors {
-    String firstName;
-    String lastName;
 
-    public Authors(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    public String getFirstName() {
-        return this.firstName;
-    }
-
-    public String getLastName() {
-        return this.lastName;
-    }
 }
--- src/Book.java
+++ src/Book.java
@@ -1,27 +1,3 @@
 public class Book {
-    String nameOfTheBook;
-    Authors author;
-    int yearOfPublish;
 
-    public Book(String nameOfTheBook, Authors author, int yearOfPublish) {
-        this.nameOfTheBook = nameOfTheBook;
-        this.author = author;
-        this.yearOfPublish = yearOfPublish;
-    }
-
-    public String getNameOfTheBook() {
-        return this.nameOfTheBook;
-    }
-
-    public String getAuthor() {
-        return this.author.getFirstName() + " " + this.author.getLastName();
-    }
-
-    public int getYearOfPublish() {
-        return this.yearOfPublish;
-    }
-
-    public void setYearOfPublish(int yearOfPublish) {
-        this.yearOfPublish = yearOfPublish;
-    }
 }
